64ee7de256939abb19a508db9c987ed5a49ca629
Artur\Traur <arkokott@gmail.com>
2019-03-12 14:26:42 +0100

    closeApp

---
+++ CloseApp/CloseApp/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloseApp
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            String answer;
+            Console.WriteLine("Close application? ");
+            answer = Console.ReadLine();
+
+            switch (answer.ToLower())
+            {
+                case "t":
+                    Console.WriteLine("Close.");
+                    break;
+                case "n":
+                    Console.WriteLine("No close.");
+                    break;
+                default:
+                    Console.WriteLine("No close deafult.");
+                    break;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
