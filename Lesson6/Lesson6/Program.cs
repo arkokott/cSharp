eba6bb20a4924f1d82dc1e131acc259ff303ad49
Artur\Traur <arkokott@gmail.com>
2019-03-13 14:06:05 +0100

    lesson 6 end

---
+++ Lesson6/Lesson6/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using csTut = csharp_station.tutorial.myExample; //alias
+
+class AliasDirective
+{
+    public static void Main()
+    {
+        //Call namespace member
+        csTut.myPrint();
+        myPrint();
+    }
+
+    static void myPrint()
+    {
+        Console.WriteLine("Not a member of csharp_station.tutorial.myExample.");
+    }
+}
+
+namespace csharp_station.tutorial
+{
+    class myExample
+    {
+        public static void myPrint()
+        {
+            Console.WriteLine("This is a member of csharp_station.tutorial.myExample.");
+        }
+    }
+}
