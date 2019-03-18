8340c03019c0e4367e3b0f5d7de80b80dbfe49a5
arkokott <arkokott@gmail.com>
2019-03-18 11:14:54 +0100

    Lesson8

--- Lesson7/Lesson7/Program.cs
+++ Lesson7/Lesson7/Program.cs
@@ -8,8 +8,36 @@ namespace Lesson7
 {
     class OutputClass
     {
-        static void Main()
+        string myString;
+
+        //Constructor
+        public OutputClass (string inputString)
         {
+            myString = inputString;
+        }
+
+        //Instance method
+        public void printString()
+        {
+            Console.WriteLine("{0}", myString);
+        }
+
+        //Destructor
+        ~OutputClass()
+        {
+            //Some resource cleanup routines
+        }
+
+        class ExampleClass
+        {
+            public static void Main()
+            {
+                //Instance of OutputClass
+                OutputClass outCl = new OutputClass("This is printed by the output class.");
+
+                //Call Output class method
+                outCl.printString();
+            }
         }
     }
 }
