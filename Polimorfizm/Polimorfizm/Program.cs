c18b8c0a2cac09a30963656b8f672f7c8ad1fe42
arkokott <arkokott@gmail.com>
2019-03-19 09:57:46 +0100

    polimorfizm v1

---
+++ Polimorfizm/Polimorfizm/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polimorfizm
+{
+    public class AnimalObject
+    {
+        public virtual void Animal()
+        {
+            Console.WriteLine("Zwierze wydaje głos.");
+        }
+    }
+
+    public class Cat : AnimalObject
+    {
+        public override void Animal()
+        {
+            Console.WriteLine("Kot miauczy.");
+        }
+    }
+
+    public class Dog : AnimalObject
+    {
+        public override void Animal()
+        {
+            Console.WriteLine("Pies szczeka.");
+        }
+    }
+
+    public class Duck : AnimalObject
+    {
+        public override void Animal()
+        {
+            Console.WriteLine("Kaczka gdacze.");
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            List<AnimalObject> AnimalList = new List<AnimalObject>();
+
+            AnimalList.Add(new Cat());
+            AnimalList.Add(new Dog());
+            AnimalList.Add(new Duck());
+
+            foreach (AnimalObject animalObj in AnimalList)
+            {
+                animalObj.Animal();
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
