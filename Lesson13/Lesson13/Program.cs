7d534e8d12c432659b92e3cb9b5cdaa1e9d85e44
arkokott <arkokott@gmail.com>
2019-03-20 10:57:17 +0100

    Lesson13

---
+++ Lesson13/Lesson13/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson13
+{
+    interface IParentInterface
+    {
+        void ParentInterfaceMethod();
+    }
+
+    interface IMyInterface : IParentInterface
+    {
+        void MethodToImplement();
+    }
+    class InterfaceImplementer : IMyInterface
+    {
+        static void Main()
+        {
+            InterfaceImplementer iImp = new InterfaceImplementer();
+            iImp.MethodToImplement();
+            iImp.ParentInterfaceMethod();
+
+            Console.ReadKey();
+        }
+
+        public void MethodToImplement()
+        {
+            Console.WriteLine("MethodToImplement() called.");
+        }
+
+        public void ParentInterfaceMethod()
+        {
+            Console.WriteLine("ParentInterfaceMethod() called.");
+        }
+    }
+}
